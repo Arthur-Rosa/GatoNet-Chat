5f3a650b2f93766ef660909a7e8f3fa41aae88d4
Arthur Rosa <93738072+Arthur-Rosa@users.noreply.github.com>
2024-05-27 01:32:19 -0300

    :8ball:

---
+++ webpack.config.mjs
@@ -0,0 +1,17 @@
+import TerserPlugin from "terser-webpack-plugin";
+
+export default {
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+          mangle: true,
+        },
+      }),
+    ],
+  },
+};
